20d0fd3c93480a4f847bc3494de6e8f98e47907d
SatyaPrakash15 <105581379+SatyaPrakash15@users.noreply.github.com>
2025-01-19 17:36:21 +0000

    String parameter learning

---
+++ Basics/01_string.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+const str=new String("My name is Sairana")
+console.log(str);
+// console.log(trim(str));
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+
+
+
+
