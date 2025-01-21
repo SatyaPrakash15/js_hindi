29c081d3ad70138b25e2bb9ef3e15faa813b51a4
SatyaPrakash15 <105581379+SatyaPrakash15@users.noreply.github.com>
2025-01-21 11:26:30 +0000

    String all things

--- Basics/01_string.js
+++ Basics/01_string.js
@@ -6,6 +6,24 @@ console.log(str);
 
 console.log(str.toUpperCase());
 console.log(str.toLowerCase());
+console.log('Here is a code for testing');
+console.log(str.charAt(3));
+console.log(str.bold());
+console.log(str.big());
+console.log(str.length);
+console.log(str.replace('name','nema'));
+console.log(str.search("name"));
+console.log(str.search(/nAme/i)); // this is for case sensitive check
+console.log(str.endsWith("a"));
+console.log(str.endsWith("A"));
+console.log(str.endsWith("m"));
+console.log(str.endsWith("M"));
+console.log(str.lastIndexOf("is"));
+
+ console.log(str.trimLeft);
+ 
+
+
 
 
 
